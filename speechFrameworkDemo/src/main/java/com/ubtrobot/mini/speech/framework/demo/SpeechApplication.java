92508b52410054bcda5f62dc30810ef4a99df349
Tsz Long Isaac Lam <isaaclam609@gmail.com>
2024-08-20 14:19:58 +0800

    Azure Speech Recognition (Speech to Text)

--- speechFrameworkDemo/src/main/java/com/ubtrobot/mini/speech/framework/demo/SpeechApplication.java
+++ speechFrameworkDemo/src/main/java/com/ubtrobot/mini/speech/framework/demo/SpeechApplication.java
@@ -7,6 +7,11 @@ import android.util.Log;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechUtility;
 import com.iflytek.cloud.msc.util.log.DebugLog;
+import com.microsoft.cognitiveservices.speech.SpeechConfig;
+import com.microsoft.cognitiveservices.speech.ServicePropertyChannel;
+import com.microsoft.cognitiveservices.speech.PropertyId;
+import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
 import com.ubtech.utilcode.utils.thread.ThreadPool;
 import com.ubtechinc.skill.SkillHelper;
 import com.ubtrobot.master.context.BaseContext;
@@ -38,6 +43,11 @@ import com.ubtrobot.ulog.FwLoggerFactory2;
 import com.ubtrobot.ulog.logger.android.AndroidLoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class SpeechApplication extends AbstractSpeechApplication {
 
@@ -50,6 +60,27 @@ public class SpeechApplication extends AbstractSpeechApplication {
     public static Context getSpeechApplicationContext() {
         return instance.getApplicationContext();
     }
+
+    // <Azure Speech to Text>
+    private static final String SpeechSubscriptionKey = "your-key";
+    private static final String SpeechRegion = "japaneast";
+    private static final String InitialSilenceTimeoutMs = "5000";
+    private static final String SegmentationSilenceTimeoutMs = "500";   // duration of silence before end of phrase
+
+    private MicrophoneStream microphoneStream;
+    private MicrophoneStream createMicrophoneStream() {
+        this.releaseMicrophoneStream();
+
+        microphoneStream = new MicrophoneStream();
+        return microphoneStream;
+    }
+    private void releaseMicrophoneStream() {
+        if (microphoneStream != null) {
+            microphoneStream.close();
+            microphoneStream = null;
+        }
+    }
+    // </Azure Speech to Text>
 
     @Override
     public void onCreate() {
@@ -79,16 +110,106 @@ public class SpeechApplication extends AbstractSpeechApplication {
                     moduleCreatedNotifier.notifyModuleCreated(DemoSpeech.INSTANCE.createSpeechService());
                 }));
 
-//        HeadEvent event = HeadEvent.newInstance();
-//        SingleClickReceiver receiver = new SingleClickReceiver() {
-//            @Override
-//            public boolean onSingleClick(KeyEvent event) {
-//                Log.d("SingleClickReceiver", "onSingleClicked:" + event);
-//                Mqtt.publishMessage("testing");
-//                return true;
-//            }
-//        };
-//        SysEventApi.get().subscribe(event, receiver);
+
+        // <Azure Speech to Text>
+
+        // create config
+        final SpeechConfig speechConfig;
+        try {
+            speechConfig = SpeechConfig.fromSubscription(SpeechSubscriptionKey, SpeechRegion);
+            speechConfig.setSpeechRecognitionLanguage("zh-HK");
+            speechConfig.setProperty(PropertyId.SpeechServiceConnection_InitialSilenceTimeoutMs, InitialSilenceTimeoutMs);
+            speechConfig.setProperty(PropertyId.Speech_SegmentationSilenceTimeoutMs, SegmentationSilenceTimeoutMs);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+
+        //Wakeup event receiver
+        SpeechApi speechApi = SpeechApi.get();
+        WakeupReceiver wakeupReceiver = new WakeupReceiver() {
+            private boolean continuousListeningStarted = false;
+            private SpeechRecognizer reco = null;
+            private AudioConfig audioInput = null;
+
+            @Override
+            public void onWakeup(Speech.WakeupParam data) {
+                Log.i("Speech Recognition", "Wakeup.");
+
+                if (continuousListeningStarted) {
+                    if (reco != null) {
+                        final Future<Void> task = reco.stopContinuousRecognitionAsync();
+                        setOnTaskCompletedListener(task, result -> {
+                            Log.i("Speech Recognition", "Continuous recognition stopped.");
+                            continuousListeningStarted = false;
+                        });
+                    } else {
+                        continuousListeningStarted = false;
+                    }
+                    return;
+                }
+
+                try {
+                    audioInput = AudioConfig.fromStreamInput(createMicrophoneStream());
+                    reco = new SpeechRecognizer(speechConfig, audioInput);
+
+                    reco.recognizing.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String recognitionResult = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i("Speech Recognition", "Intermediate result received: " + recognitionResult);
+
+                    });
+
+                    reco.recognized.addEventListener((o, speechRecognitionResultEventArgs) -> {
+                        final String recognitionResult = speechRecognitionResultEventArgs.getResult().getText();
+                        Log.i("Speech Recognition", "Final result received: " + recognitionResult);
+
+                        // stop continuous recognition (if not it keeps recognizing infinitely)
+                        final Future<Void> task = reco.stopContinuousRecognitionAsync();
+                        setOnTaskCompletedListener(task, result -> {
+                            Log.i("Speech Recognition", "Continuous recognition stopped.");
+                            continuousListeningStarted = false;
+                        });
+
+                        if (!Objects.equals(recognitionResult, "")) {
+                            // text generation
+                            // tts and play
+                        }
+
+                    });
+
+                    final Future<Void> task = reco.startContinuousRecognitionAsync();
+                    setOnTaskCompletedListener(task, result -> {
+                        continuousListeningStarted = true;
+                    });
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        };
+        speechApi.subscribeEvent(wakeupReceiver);
+
+        // </Azure Speech To Text>
 
     }
+
+
+    // <Azure Speech to Text>
+    private <T> void setOnTaskCompletedListener(Future<T> task, OnTaskCompletedListener<T> listener) {
+        s_executorService.submit(() -> {
+            T result = task.get();
+            listener.onCompleted(result);
+            return null;
+        });
+    }
+
+    private interface OnTaskCompletedListener<T> {
+        void onCompleted(T taskResult);
+    }
+
+    private static ExecutorService s_executorService;
+    static {
+        s_executorService = Executors.newCachedThreadPool();
+    }
+    // </Azure Speech to Text>
 }
